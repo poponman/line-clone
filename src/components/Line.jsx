122c1417e09f2378ac6b7478533579d0821fbf8a
poponman <yuripon1221@gmail.com>
2025-05-29 15:31:23 +0900

    hookを用いたデータベースからのデータの取得

--- src/components/Line.jsx
+++ src/components/Line.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SignOut from './SignOut.jsx';
+import { db } from '../firebase.js';
 
 function Line() {
+    const[messages, setMessages] = useState();
+    useEffect(() => {
+        db.collection('messages')
+        .orderBy('createdAt')
+        .limit(50)
+        .onSnapshot((snapshot) => {
+            setMessages(snapshot.docs.map((doc) => doc.data()) )
+        });
+    },[]);//最初のマウント時のみ読み込み
+
   return (
     <div>
         <SignOut />
