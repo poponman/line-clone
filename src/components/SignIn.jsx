ca6dbb00bfdd6fddad260bf0a4ed3be00cfcc79c
poponman <yuripon1221@gmail.com>
2025-05-29 13:41:21 +0900

    サインインのためのbuttonとfunction(中身無し)を追加

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Button } from '@mui/material';
 
 function SignIn() {
+    function signInWithGoogle(){
+
+    }
+    
   return (
     <div>
-        
+        <Button onClick = {signInWithGoogle}> 
+            グーグルでログインする
+        </Button>
     </div>
   )
 }
