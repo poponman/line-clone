8ba4e51774e3f5202d5a904b14a39b68236f8b29
poponman <yuripon1221@gmail.com>
2025-06-01 16:09:35 +0900

    headerの追加

--- src/components/SIgnOut.jsx
+++ src/components/SIgnOut.jsx
@@ -1,13 +1,18 @@
 import { Button } from '@mui/material'
 import { auth } from '../firebase'
+import CallIcon from '@mui/icons-material/Call';
 import React from 'react'
 
 function SignOut() {
   return (
-    <div>
-        <Button onClick={() => auth.signOut()}>
+    <div className='header'>
+        <Button 
+          style={{color: 'white',fontSize: '15px'}}
+        onClick={() => auth.signOut()}>
             サインアウト
         </Button>
+        <h3>{auth.currentUser.displayName}</h3>
+        <CallIcon />
     </div>
   )
 }
